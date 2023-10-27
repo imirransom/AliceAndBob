9371499e00687ca4f4016f635b5eb092dc885440
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-27 03:51:27 -0400

    started lab

---
+++ src/main/java/Name.java
@@ -0,0 +1,13 @@
+/**
+ * Created by iyasuwatts on 10/17/17.
+ */
+import java.util.Scanner;
+public class Name {
+
+    public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! What is your name? ");
+        String name = scanner.nextLine();
+
+    }
+}
