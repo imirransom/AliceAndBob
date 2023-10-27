568ad1bfa08585cb3f241fe5cab6b0bb233e1170
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-10-27 04:20:40 -0400

    seeing if I should keep the quit program

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -14,14 +14,18 @@ public class Name {
         String name = scanner.nextLine();
         //declaring the variable to name.toLowerCase() will make it so even if the user
         //uses lower case to input their name or upper case, the input will be accepted and return
-        //true since
+        //true since it will return lower case no matter the users camile casing
         name = name.toLowerCase();
 
         while (true){
-            if (name == "alice" || name == "bob") {
+            //using the equal. method to test strings for equality instead of "=="
+            if (name.equals("alice") || name.equals("bob")) {
                 System.out.println("Hello " +name+ "! Welcome back.");
-            } else if (name.equalsIgnoreCase("")) {
-                System.out.println("Sorry, you are not Alice nor Bob!");
+                break;
+            } else {
+                System.out.println("Sorry, you are not Alice nor Bob! Program will now shut down.");
+                //This quits/exits the program
+                System.exit(0);
             }
         }
     }
